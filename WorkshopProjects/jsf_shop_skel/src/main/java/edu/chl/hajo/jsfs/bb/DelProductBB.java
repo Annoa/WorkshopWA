941b31ccac3bd3190061bcf5ce901fdae3e796ff
Krabban91 <anderssongabriel665@gmail.com>
2013-09-18 17:05:01 +0200

    Fixed frontend. ie. 2.1

--- WorkshopProjects/jsf_shop_skel/src/main/java/edu/chl/hajo/jsfs/bb/DelProductBB.java
+++ WorkshopProjects/jsf_shop_skel/src/main/java/edu/chl/hajo/jsfs/bb/DelProductBB.java
@@ -4,9 +4,7 @@
  */
 package edu.chl.hajo.jsfs.bb;
 
-import edu.chl.hajo.jsfs.mb.Shop;
-import edu.chl.hajo.shop.core.Product;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Named;
 
 /**
@@ -14,48 +12,12 @@ import javax.inject.Named;
  * @author Gabriel
  */
 @Named("delProduct")
-@RequestScoped
-public class DelProductBB {
+@ConversationScoped
+public class DelProductBB extends ConversationalBase{
 
-    private Product product;
-    private String name;
-    private String price;
-    private String id;
-    private Shop shop = Shop.INSTANCE;
-
-    public void setSelected(Long id) {
-        this.product = shop.getProductCatalogue().find(id);
-        this.id = product.getId().toString();
-        this.name = product.getName();
-        this.price = "" + product.getPrice();
+    @Override
+    protected void execute() {
+        getProductCatalogue().remove(getId());
     }
-
-    public String delete() {
-        shop.getProductCatalogue().remove(product.getId());
-        return Navigation.PRODUCT_SUCCESS.toString();  // TODO
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
+    
 }
--- WorkshopProjects/jsf_shop_skel/src/main/java/edu/chl/hajo/jsfs/bb/EditProductBB.java
+++ WorkshopProjects/jsf_shop_skel/src/main/java/edu/chl/hajo/jsfs/bb/EditProductBB.java
@@ -4,9 +4,8 @@
  */
 package edu.chl.hajo.jsfs.bb;
 
-import edu.chl.hajo.jsfs.mb.Shop;
 import edu.chl.hajo.shop.core.Product;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Named;
 
 /**
@@ -14,48 +13,14 @@ import javax.inject.Named;
  * @author Gabriel
  */
 @Named("editProduct")
-@RequestScoped
-public class EditProductBB {
-    
-    private Product product;
-    private String name;
-    private String price;
-    private String id;
-    private Shop shop = Shop.INSTANCE;
-    
-    public void setSelected(Long id){
-        this.product = shop.getProductCatalogue().find(id);
-        this.id = product.getId().toString();
-        this.name = product.getName();
-        this.price = ""+product.getPrice();
-    }
-    
-    public String update() {
-        
-        shop.getProductCatalogue().add(new Product(product.getId(), name, Double.parseDouble(price)));
-        //TODO: update product with new name and price. 
-        //shop.getProductCatalogue().update(product);
-        return Navigation.PRODUCT_SUCCESS.toString(); 
-    }
-    
-    public String getName() {
-        return name;
-    }
-    public String getId() {
-        return id;
-    }
-    public String getPrice() {
-        return price;
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setId(String id){
-        this.id = id;
-    }
-    public void setPrice(String price){
-        this.price = price;
-    }
+@ConversationScoped
+public class EditProductBB extends ConversationalBase{
 
+    @Override
+    protected void execute() {
+        Product p = new Product(this.getId(), this.getName(), Double.parseDouble(this.getPrice()));
+        getProductCatalogue().update(p);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
